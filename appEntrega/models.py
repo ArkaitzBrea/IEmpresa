7ac08136b8d6b5836eb678ca86449bf12a164929
IbaiCarreno <carreno.ibai@opendeusto.es>
2021-04-23 16:28:23 +0200

    Primera tabla en Models.py (CLIENTE)

--- appEntrega/models.py
+++ appEntrega/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Cliente(models.Model):
+    cif = models.CharField(max_length=12, primary_key=True)
+    nombre_empresa = models.CharField(max_length=40)
+    email = models.EmailField(max_length=254)
+    telefono = models.IntegerField(max_length=9, unique=True)
+    nombre_cliente = models.CharField(max_length=20)
