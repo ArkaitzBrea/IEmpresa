1568c2f816166842cf72505d012b810ddc3817b9
Arkaitz <arkaitz.brea@opendeusto.es>
2021-04-23 16:44:49 +0200

    prueba de push Arkaitz

--- appEntrega/models.py
+++ appEntrega/models.py
@@ -1,9 +1,11 @@
 from django.db import models
 
-
+#Tabla cliente
 class Cliente(models.Model):
     cif = models.CharField(max_length=12, primary_key=True)
     nombre_empresa = models.CharField(max_length=40)
     email = models.EmailField(max_length=254)
     telefono = models.IntegerField(max_length=9, unique=True)
     nombre_cliente = models.CharField(max_length=20)
+
+
