cbba016dc26d950903cc9e5a28cee4869a84b3fb
Arkaitz <arkaitz.brea@opendeusto.es>
2021-04-28 20:29:32 +0200

    formulario

--- appEntrega/urls.py
+++ appEntrega/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
-from appEntrega.views import PedidoDetailView, PedidoListView,CreateNoticiaView
+from appEntrega.views import PedidoDetailView, PedidoListView,CreateProductoView
 
 urlpatterns = [
     path('pedidos/<int:pk>', PedidoDetailView.as_view(), name='detallePedido'),
