66df818f5bef8ddbf86d19130dda330b64130aca
IbaiCarreno <carreno.ibai@opendeusto.es>
2021-04-29 17:00:21 +0200

    asociar y añadir estilos

--- appEntrega/admin.py
+++ appEntrega/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
-from .models import Cliente, Producto, Componente, Orden_Pedido, Orden_Linea
+from .models import Cliente, Producto, Componente, Orden_Pedido, Factura
 
 # Aquí se registran los modelos/tablas
 admin.site.register(Cliente)
 admin.site.register(Producto)
 admin.site.register(Componente)
 admin.site.register(Orden_Pedido)
-admin.site.register(Orden_Linea)
+admin.site.register(Factura)
