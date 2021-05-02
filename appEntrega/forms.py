9b9b9096e44c823b320dd58a8395e13000a72973
IbaiCarreno <carreno.ibai@opendeusto.es>
2021-05-02 17:39:54 +0200

    css base - grid bootstrap

--- appEntrega/forms.py
+++ appEntrega/forms.py
@@ -8,20 +8,7 @@ class ProductoForm(forms.ModelForm):
         model = Producto
         fields = '__all__'
 
-    producto_referencia = forms.CharField(widget=forms.TextInput(
-        attrs={'class': 'formulario__input'}))
-    producto_nombre = forms.CharField(widget=forms.TextInput(
-        attrs={'class': 'formulario__input'}))
-    producto_descripcion = forms.CharField(widget=forms.TextInput(
-        attrs={'class': 'formulario__input'}))
-    producto_categoria = forms.CharField(widget=forms.TextInput(
-        attrs={'class': 'formulario__input'}))
-    producto_precio = forms.DecimalField(widget=forms.NumberInput(
-        attrs={'class': 'formulario__input'}))
-
 # Formulario nuevo cliente
-
-
 class ClienteForm(forms.ModelForm):
     class Meta:
         model = Cliente
@@ -29,8 +16,6 @@ class ClienteForm(forms.ModelForm):
 
 
 # Formulario nuevo componente
-
-
 class ComponenteForm(forms.ModelForm):
     class Meta:
         model = Componente
@@ -38,16 +23,12 @@ class ComponenteForm(forms.ModelForm):
 
 
 # Formulario nuevo pedido
-
-
 class PedidoForm(forms.ModelForm):
     class Meta:
         model = Orden_Pedido
         fields = '__all__'
 
 # Formulario nueva  factura
-
-
 class FacturaForm(forms.ModelForm):
     class Meta:
         model = Factura
