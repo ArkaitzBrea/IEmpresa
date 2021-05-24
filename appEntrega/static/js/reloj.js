342fd186f671f17212b3bbafc58e18b586f2022e
IbaiCarreno <carreno.ibai@opendeusto.es>
2021-05-24 19:49:36 +0200

    reloj

--- appEntrega/static/js/reloj.js
+++ appEntrega/static/js/reloj.js
@@ -2,6 +2,6 @@ const horaContainer = document.querySelector('.fecha')
 
 const actualizarHora = setInterval(function () {
     const date = new Date()
-    horaContainer.innerHTML = ` ${date.getUTCDate()} / ${date.getUTCMonth()} / ${date.getFullYear()} -  ${date.getUTCHours()+2} : ${date.getUTCMinutes()}`
+    horaContainer.innerHTML = ` ${date.getUTCDate()} / ${date.getUTCMonth() + 1} / ${date.getFullYear()} -  ${date.getUTCHours() + 2} : ${date.getUTCMinutes()}`
 
 }, 1000)
